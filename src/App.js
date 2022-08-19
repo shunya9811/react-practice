8161cb0a00e501416f9449e0efdc9dfd88581cc9
shunya9811 <11wash27@gmail.com>
2022-08-19 17:47:30 +0900

    count up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import TodoList from './components/TodoList';
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  useEffect(() => {
+    console.log('useEffectが実行されました。')
+    
+  }, [count])
+
   return (
-    <div style={{ margin: "2em"}}>
-      <TodoList/>
+    <div className="App">
+      <h1>Learn useEffect</h1>
+      <h2>Count: { count } / Count2: { count2 }</h2>
+      <button onClick={() => setCount(count+1)}>Count+</button><br/>
+      <button onClick={() => setCount2(count2+1)}>Count2+</button><br/>
     </div>
   );
 }
